a8e1866f61d44763be57b939f15d7d2a8eabd042
Aid Puska <aidpuska29@gmail.com>
2023-01-30 14:43:22 +0100

    Deleted unnecessary elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import { motion } from 'framer-motion'
 import { useEffect } from 'react'
 import { useRef, useState } from 'react'
-/* import { NavLink, Link } from 'react-router-dom' */
 
 const Navbar = ({ projectRef, footerRef, skillsRef }) => {
 
